8514514eb3cb34e0c5cf90dbc337107ae7ef12ca
Robin Millette <robin@millette.info>
2018-09-06 05:39:56 -0400

    Barchart titles.

--- components/barchart.js
+++ components/barchart.js
@@ -2,52 +2,63 @@
 import React, { Component } from 'react'
 import VegaLite from 'react-vega-lite'
 
-const spec = {
-  width: 550,
-  height: 450,
-  config: {
-    autosize: 'pad',
-    axis: {
-      labelFontSize: 20,
-      titleFontSize: 20
-    },
-    legend: {
-      labelFontSize: 20,
-      titleFontSize: 20
-    }
-  },
-  mark: 'bar',
-  encoding: {
-    color: { field: 'label', type: 'nominal' },
-    x: {
-      field: 'label',
-      type: 'nominal',
-      sort: {
-        field: 'personnes'
-      },
-      axis: {
-        labelAngle: 45
-      }
-    },
-    y: {
-      field: 'personnes',
-      type: 'quantitative',
-      axis: {
-        titlePadding: 15
-      }
-    }
-  }
-}
-
 class Barchart extends Component {
   constructor (props) {
     super(props)
+    const { title, children } = props
     const re = /([1-9]\. .+?): \*\*(.+?)\*\* (.*?)\s/g
+
+    this.spec = {
+      width: 550,
+      height: 450,
+      config: {
+        autosize: 'pad',
+        axis: {
+          labelFontSize: 20,
+          titleFontSize: 20
+        },
+        legend: {
+          labelFontSize: 20,
+          titleFontSize: 20
+        }
+      },
+      mark: 'bar',
+      encoding: {
+        color: {
+          sort: {
+            field: 'personnes'
+          },
+          title,
+          field: 'label',
+          type: 'nominal'
+        },
+        x: {
+          title,
+          field: 'label',
+          type: 'nominal',
+          sort: {
+            field: 'personnes'
+          },
+          axis: {
+            labelAngle: 45
+          }
+        },
+        y: {
+          title: 'Personnes',
+          field: 'personnes',
+          type: 'quantitative',
+          axis: {
+            titlePadding: 15
+          }
+        }
+      }
+    }
+
 
     this.barData = []
     let oy
     let personnes
-    while ((oy = re.exec(props.children + ' '))) {
+    while ((oy = re.exec(children + ' '))) {
       let [, label, personnesImp, word] = oy
       label = label.slice(3)
       personnes = parseInt(personnesImp.replace(/\s/, ''), 10)
@@ -57,7 +68,7 @@ class Barchart extends Component {
     this.list = (
       <ol style={{ textAlign: 'left', fontSize: '1.5em' }}>
         {this.barData.map(({ label, personnesImp, word }) => (
-          <li key={label}>{label.slice(2)}: <b>{personnesImp}</b> {word}</li>
+          <li key={label}>{label}: <b>{personnesImp}</b> {word}</li>
         ))}
       </ol>
     )
@@ -74,10 +85,9 @@ class Barchart extends Component {
 
   render () {
     const props = {
-      spec,
+      spec: this.spec,
       data: { values: this.barData }
     }
-    // {this.state.graphic ? <VegaLite spec={spec} data={{ values: this.barData }} /> : this.list}
     return (
       <div style={{ cursor: 'pointer' }} onClick={this.click}>
         {this.state.graphic ? <VegaLite { ...props } /> : this.list}
